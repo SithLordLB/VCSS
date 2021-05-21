6002edd6017bc705dedf1368f610781ac635a74c
Julian Helperstorfer <julian.helperstorfer@htl-wels.at>
2021-05-21 13:06:41 +0200

    1.5|     added the list     changed the getCourse()-method so it looks for the date too

--- src/sample/CourseList.java
+++ src/sample/CourseList.java
@@ -3,7 +3,7 @@ package sample;
 /*
     Author: Julian Helperstorfer
     Created on: 17.05.2021
-    Changed on: 17.05.2021
+    Changed on: 21.05.2021
     Changed from:
     Description: Class for the list that stores all the courses
  */
@@ -13,16 +13,19 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class CourseList {
+
+    private static ArrayList<Course> courseList = new ArrayList<>();    //the list in which the courses are stored
+
     public void update() throws IOException {
         //Filling the list with courses
     }
 
-    public static double getKurswert(Currency startCurrency, Currency targetCurrency, ArrayList<Course> list){  //method for finding a fitting course for a start- and a target currency
+    public static double getCourse(Currency startCurrency, Currency targetCurrency, Date date){  //method for finding a fitting course for a start- and a target currency
         Course tempCourse = new Course(startCurrency,targetCurrency, 0, new Date());    //temporary Course
 
-        for (Course course : list) {    //search the list for a fitting course
+        for (Course course : courseList) {    //search the list for a fitting course
 
-            if (startCurrency.equals(course.getCurrency1()) && targetCurrency.equals(course.getCurrency2())) {
+            if (startCurrency.equals(course.getCurrency1()) && targetCurrency.equals(course.getCurrency2()) && date.equals(course.getDate())) {
                 tempCourse = course;    //set the fitting course as the temporary course
             }
 
@@ -33,5 +36,10 @@ public class CourseList {
         } else{
             return 0;
         }
+    }
+
+
+    public static ArrayList<Course> getCourseList() {
+        return courseList;
     }
 }
